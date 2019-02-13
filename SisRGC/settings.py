c006f8b7409857c32396925eecbad7e89fb4420b
Gustavo Carlos Couto <gustavo.couto@ibram.gdfnet.df>
2019-02-13 17:43:27 -0200

    fixing a bug

--- SisRGC/settings.py
+++ SisRGC/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['10.44.2.7','localhost', 'sisrgc.herokuapp.com/']
+ALLOWED_HOSTS = ['10.44.2.7','localhost', 'sisrgc.herokuapp.com']
 
 
 # Application definition
