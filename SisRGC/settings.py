b61f06d086f6c1d5785fe22aaa71306a9573735f
Gustavo Carlos Couto <gustavo.couto@ibram.gdfnet.df>
2019-02-13 16:53:23 -0200

    Confihurando o Heroku

--- SisRGC/settings.py
+++ SisRGC/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+from decouple import config
+from dj_database_url import parse as dburl
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!pe3ts16vlk46-h=@p4tm5imryow)s(i1w@f@kzj*j2)60v!xi'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['10.44.2.7','localhost']
+ALLOWED_HOSTS = ['10.44.2.7','localhost', 'sisrgc.herokuapp.com/']
 
 
 # Application definition
@@ -74,12 +75,9 @@ WSGI_APPLICATION = 'SisRGC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
 
 
 # Password validation
@@ -123,3 +121,5 @@ STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
 
 LOGIN_REDIRECT_URL = 'listagem'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
